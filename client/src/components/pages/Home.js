1a6c8abac7ac53e6de400ca210c6a37e56671f12
Abdeljalil Misbah <iberomaurusian@Abdeljalils-MacBook-Pro.local>
2020-10-14 23:17:45 +0300

    Initial commit

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -18,12 +18,12 @@ function Map() {
 
   });
 
-  const [selectedPark, setSelectedPark] = useState(null);
+  const [selectedStation, setselectedStation] = useState(null);
 
   useEffect(() => {
     const listener = e => {
       if (e.key === "Escape") {
-        setSelectedPark(null);
+        setselectedStation(null);
       }
     };
     window.addEventListener("keydown", listener);
@@ -45,17 +45,17 @@ function Map() {
           setViewport(viewport);
         }}
       >
-        {stationData.features.map(park => (
+        {stationData.features.map(station => (
           <Marker
-            key={park.properties.PARK_ID}
-            latitude={park.geometry.coordinates[1]}
-            longitude={park.geometry.coordinates[0]}
+            key={station.properties.STATION_ID}
+            latitude={station.geometry.coordinates[1]}
+            longitude={station.geometry.coordinates[0]}
           >
             <div
               className="marker-btn"
               onClick={e => {
                 e.preventDefault();
-                setSelectedPark(park);
+                setselectedStation(station);
               }}
             >
               <img src="/gas-station.svg" alt="Station Icon" />
@@ -63,45 +63,45 @@ function Map() {
           </Marker>
         ))}
 
-        {selectedPark ? (
+        {selectedStation ? (
           <Popup
-            latitude={selectedPark.geometry.coordinates[1]}
-            longitude={selectedPark.geometry.coordinates[0]}
+            latitude={selectedStation.geometry.coordinates[1]}
+            longitude={selectedStation.geometry.coordinates[0]}
             onClose={() => {
-              setSelectedPark(null);
+              setselectedStation(null);
             }}
           >
             <div>
-              <h3>{selectedPark.properties.NAME}</h3>
-              <p>{selectedPark.properties.ADDRESS}{selectedPark.properties.DESCRIPTIO.map(plugs => <ul>{plugs}</ul>)}</p>
+              <h3>{selectedStation.properties.NAME}</h3>
+              <p>{selectedStation.properties.ADDRESS}{selectedStation.properties.DESCRIPTIO.map(plugs => <ul>{plugs}</ul>)}</p>
             </div>
           </Popup>
         ) : null}
-        {selectedPark ? (
+        {selectedStation ? (
           <div className='sidebar'>
 
             <div className='heading'>
               <button onClick={login}>check In</button>
-              <img src={selectedPark.properties.IMG} alt="image lost" />
+              <img src={selectedStation.properties.IMG} alt="photo lost" />
             </div>
 
             <div className='title'>
-              <h3>{selectedPark.properties.NAME}</h3>
-              <div className="info"><p>{selectedPark.properties.ADDRESS}</p>{selectedPark.properties.DESCRIPTIO.map(plugs => <ul>{plugs}</ul>)} <p>{selectedPark.properties.CAPACITY}</p></div>
+              <h3>{selectedStation.properties.NAME}</h3>
+              <div className="info"><p>{selectedStation.properties.ADDRESS}</p>{selectedStation.properties.DESCRIPTIO.map(plugs => <ul>{plugs}</ul>)} <p>{selectedStation.properties.CAPACITY}</p></div>
             </div>
 
             <div className='listings'>
               <h4>Cost per minutes</h4>
-              <p className="list">{selectedPark.properties.PRICE}</p>
+              <p className="list">{selectedStation.properties.PRICE}</p>
               <h4>Amenities</h4>
-              <p className="list">{selectedPark.properties.FACILITY}</p>
+              <p className="list">{selectedStation.properties.FACILITY}</p>
               <h4>Hours</h4>
-              <p className="list">{selectedPark.properties.HOURS}</p>
+              <p className="list">{selectedStation.properties.HOURS}</p>
             </div>
 
             <div className="plugs" >
               <h4>Plugs</h4>
-              {selectedPark.properties.DESCRIPTIO.map(plugs => <ul>{plugs}</ul>)}
+              {selectedStation.properties.DESCRIPTIO.map(plugs => <ul>{plugs}</ul>)}
             </div>
 
           </div>
