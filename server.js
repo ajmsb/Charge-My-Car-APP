7e197ccd3cee445745813a1d85496eb105da5f2d
Abdeljalil Misbah <iberomaurusian@Abdeljalils-MacBook-Pro.local>
2020-10-20 05:18:05 +0300

    modified for deployement

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use("/users", require("./routes/userRouter"));
 // 🔥 serve static assets if in progress
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')));
 }
