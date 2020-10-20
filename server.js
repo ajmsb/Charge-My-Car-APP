4c80fcc0cd7f014feceaf6a6d3f9e185a16794f2
Abdeljalil Misbah <iberomaurusian@Abdeljalils-MacBook-Pro.local>
2020-10-20 05:00:32 +0300

    modified for deployement

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 require("dotenv").config({ path: './config/.env' });
 
 // 🔥 Express setup
@@ -22,7 +22,5 @@ app.use("/users", require("./routes/userRouter"));
 // 🔥 serve static assets if in progress
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
