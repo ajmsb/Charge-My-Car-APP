6d527f823dfb3525279b6a1456a47ad98e12404b
Abdeljalil Misbah <iberomaurusian@Abdeljalils-MacBook-Pro.local>
2020-10-20 05:33:44 +0300

    another commit for deployement

---
+++ client/build/precache-manifest.0b86614217d4a9096de980ed7c936146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067a7f9566597d2fca0e6def70f670c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "356c7f4af1549cd9417e",
+    "url": "/static/css/main.493a80f5.chunk.css"
+  },
+  {
+    "revision": "156a4631b5d7bfcfc15e",
+    "url": "/static/js/2.b8904437.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/static/js/2.b8904437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356c7f4af1549cd9417e",
+    "url": "/static/js/main.27329ba2.chunk.js"
+  },
+  {
+    "revision": "aaacf1ca76a1849b15bc",
+    "url": "/static/js/runtime-main.5bf7cee2.js"
+  }
+]);
